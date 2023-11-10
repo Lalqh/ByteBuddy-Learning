6eafafc955d4c43a3c18942475bacef535c70a45
Lalqh <lalincotero13@gmial.com>
2023-11-10 15:58:47 -0600

    Updated the registry with the new help files

--- models/register.php
+++ models/register.php
@@ -1,11 +1,15 @@
 <?php
 
-require_once 'conector.php';
-require_once 'Utils.php';
+require_once './Database/conector.php';
+require_once './Database/utils.php';
+require_once './Generals/helpers.php';
+require_once './Generals/responses.php';
 
 $db = Db::getInstance();
 $conexion = $db->getConnection();
 $crud = new Crud($conexion);
+$helper = new Helpers();
+$response = new Responses();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
@@ -20,23 +24,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $encrypt = password_hash($password, PASSWORD_BCRYPT, $options);
 
     if (getExistUser($email, $crud)) {
-        $response = array('respuesta' => 'registrado');
+        $response->sendResponse("exist", "Este usuario ya existe, inicia sesión o crea otro usuario");
     } else {
         $registerOk = registerUser($name, $email, $encrypt, $crud);
         if ($registerOk) {
-            $response = array('respuesta' => 'correcto');
+            $response->sendResponse("ok", "Te has registrado de manera correcta");
         } else {
-            $response = array('respuesta' => 'error');
+            $response->sendResponse("error", "Ocurrio un error");
         }
     }
-
-    echo json_encode($response);
 } else {
-    $response = array('respuesta' => 'error');
-    echo json_encode($response);
+    $response->sendResponse("error", "Ocurrio un error");
 }
 
-function getExistUser($email, $crud) {
+function getExistUser($email, $crud)
+{
     $email = $crud->getDbConnection()->real_escape_string($email); // Escapar el valor para prevenir SQL injection
     try {
         $result = $crud->select('COUNT(*) as count', 'usuarios', "correo = '$email'");
@@ -48,32 +50,18 @@ function getExistUser($email, $crud) {
         $row = $result->fetch_assoc();
         return $row['count'] > 0; // Si count es mayor que 0, el usuario existe
     } catch (Exception $e) {
-        echo "Error usuarioExiste: " . $e->getMessage();
-        return false;
+        return $response->sendResponse("error", "Ocurrio un error");
     }
 }
 
 function registerUser($name, $email, $password, $crud) {
     $dataUser = [
-        'id' => generateUuidv4(),
+        'id' => $helper->generateUuidv4(),
         'correo' => $email,
         'contrasena' => $password,
         'nombre' => $name
     ];
-
     $insertData = $crud->insert('usuarios', $dataUser);
     return $insertData;
 }
-
-function generateUuidv4()
-{
-    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-    mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-    mt_rand(0, 0xffff),
-    mt_rand(0, 0x0fff) | 0x4000,
-    mt_rand(0, 0x3fff) | 0x8000,
-    mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-    );
-}
-
 ?>
