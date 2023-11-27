0a25ff870f916c987e76a0b4ad6207f91e952482
alexmm23 <alejandromontes393@gmail.com>
2023-11-27 09:04:09 -0600

    Correcion ruta maestro

--- js/Generals/authManger.js
+++ js/Generals/authManger.js
@@ -6,7 +6,7 @@ export const reditect = (typeUser) => {
       window.location.href = "./views/Usuario/index.html";
       break;
     case 2:
-      window.location.href = "./views/Maestro/index.html";
+      window.location.href = "./Maestro/index.html";
       break;
     case 3:
       window.location.href = "../views/Admin/index.html";
@@ -20,19 +20,19 @@ export const saveData = (jwt, typeUser) => {
   console.log(jwt)
 };
 
-export const checkTypeUser = () =>{
+export const checkTypeUser = () => {
   const pageTitle = document.title;
   const typeUser = getType();
 
-  if (pageTitle === "Usuarios" && typeUser!=1){
+  if (pageTitle === "Usuarios" && typeUser != 1) {
     reditectTo(typeUser);
   }
 
-  if (pageTitle === "Profesor" && typeUser!=2){
+  if (pageTitle === "Profesor" && typeUser != 2) {
     reditectTo(typeUser);
   }
 
-  if (pageTitle === "Administrador" && typeUser!=3){
+  if (pageTitle === "Administrador" && typeUser != 3) {
     reditectTo(typeUser);
   }
 }
@@ -53,4 +53,3 @@ const reditectTo = (typeUser) => {
       break;
   }
 }
- 
