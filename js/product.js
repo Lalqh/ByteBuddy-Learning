7215a3d1e48e6bdafbe51add95aca09781a328b0
alexmm23 <alejandromontes393@gmail.com>
2023-12-03 12:34:43 -0600

    Completar vista individual de curso

--- js/product.js
+++ js/product.js
@@ -3,7 +3,17 @@ const currentURL = window.location.href;
 const urlSearchParams = new URLSearchParams(new URL(currentURL).search);
 let data = new FormData();
 data.append('course_id', urlSearchParams.get('curso_id'));
-data.append('req', 'get_course')
+data.append('req', 'get_course');
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let img = document.querySelector("#img_course");
+let price = document.querySelector("#product_price");
+let title = document.querySelector("#product_title");
+let description = document.querySelector("#product_description");
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange();//calling the function(optional)
+});
 
 postData('../../models/courses.php', data)
     .then((resp) => {
@@ -17,7 +27,14 @@ postData('../../models/courses.php', data)
                         window.location.href = './index.html'
                     }, 2000)
                 )
+            } else {
+                img.src = resp.data[0].img_src;
+                title.innerHTML = resp.data[0].nombre;
+                price.innerHTML = resp.data[0].precio;
+                description.innerHTML = resp.data[0].descripcion;
+                fillStars(resp.data[0].estrellasCurso)
             }
+
             console.log(resp.message)
         } else {
             Swal.fire(
@@ -27,3 +44,18 @@ postData('../../models/courses.php', data)
             )
         }
     })
+function fillStars(cal) {
+    // Obtener el contenedor de calificación
+    const ratingContainer = document.getElementById("ratingContainer");
+
+    // Redondear la calificación al número entero más cercano
+    const calificacionRedondeada = Math.round(cal);
+
+    // Llenar las estrellas
+    for (let i = 0; i < 5; i++) {
+        const star = document.createElement("span");
+        star.className = "rating-star" + (i < calificacionRedondeada ? "" : "-empty");
+        star.innerHTML = "&#9733;";
+        ratingContainer.appendChild(star);
+    }
+}
