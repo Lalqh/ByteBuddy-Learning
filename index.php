498a2985bc4c3d6734a0fcf7f2e5139f4ee77844
Lalqh <lalincotero13@gmail.com>
2024-05-05 00:40:22 -0600

    Correcion de rutas y se agrego el icono

--- index.php
+++ index.php
@@ -7,12 +7,17 @@
   <title>ByteBuddy-Learning</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="styles/index.css">
+  <link
+      rel="icon"
+      href="../assets/images/Logo ByteBuddy Learning.png"
+      type="image/x-icon"
+    />
 </head>
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
     <div class="container">
-      <a class="navbar-brand" href="../ByteBuddy-Learning/index2.html"><img src="../ByteBuddy-Learning/assets/images/Logo ByteBuddy Learning.png" alt=""></a>
+      <a class="navbar-brand" href="../ByteBuddy-Learning/index2.html"><img src="../assets/images/Logo ByteBuddy Learning.png" alt=""></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
