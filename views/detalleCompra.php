11a457bf4ba4c7ed28756bc83b1af2c4cd7151fb
Angel Reyes <a22110083@ceti.mx>
2024-05-05 08:21:21 -0600

    Se agrego icono a las pestañas de las vistas.

--- views/detalleCompra.php
+++ views/detalleCompra.php
@@ -15,6 +15,11 @@
 
   <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="../styles/detalleCompra.css" />
+  <link
+      rel="icon"
+      href="../assets/images/Logo ByteBuddy Learning.png"
+      type="image/x-icon"
+    />
 
 </head>
 
