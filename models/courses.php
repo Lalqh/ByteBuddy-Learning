1ae7c103b0448137d2108994efd31dc033423765
lalqh <lalincotero13@gmail.com>
2024-05-13 20:07:38 -0600

    Dbugeando query

--- models/courses.php
+++ models/courses.php
@@ -124,6 +124,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 // Generar el PDF
                 $userData = $crud->select('correo', 'usuarios', "id = '$idUser'");
                 
+                var_dump($userData);
+                exit();
+
                 $url = $pdf->createPdf($html, $idUser, $userData[0]['correo']);
 
                 if($url == false){
